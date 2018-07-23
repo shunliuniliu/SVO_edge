f9efee1ed503dcbda09c36add5b52bc279530d25
poiwu <you@example.com>
2018-07-23 21:30:37 +0800

    [Add edgelet and systrace in this commit]

---
+++ Thirdparty/Trace/trace.cpp
@@ -0,0 +1,14 @@
+
+#include "trace.h"
+
+int Tracer::atrace_marker_fd = -1;
+
+ void Tracer::trace_init()
+{
+	atrace_marker_fd = -1;
+	atrace_marker_fd = open("/sys/kernel/debug/tracing/trace_marker", O_WRONLY);
+  if (atrace_marker_fd == -1)
+  { /* do error handling */
+	 printf("Error Opening Trace file");
+  }
+}
---
+++ build_opencv.sh
@@ -0,0 +1,22 @@
+
+cd Thirdparty/sophus/
+mkdir build
+cd build
+cmake ..
+make -j
+
+
+
+cd ../../g2o
+mkdir build
+cd build
+cmake ..
+make -j
+
+
+cd ../../opencv-2.4.13/
+mkdir build
+cd build
+cmake -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=/Thirdparty/opencv-2.4.13
+make -j6
+sudo make install
